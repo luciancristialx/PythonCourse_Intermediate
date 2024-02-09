69887992297568591c9c6804fe31d6aea01b7f25
luciancristialx <alexandruluciancristi@gmail.com>
2024-02-09 15:18:44 +0200

    Added logic for reading & updating the high score as the game start / player achieves higher score

--- Day20_Day21_Day24_SnakeGame_project/scoreboard.py
+++ Day20_Day21_Day24_SnakeGame_project/scoreboard.py
@@ -3,24 +3,37 @@ ALIGNMENT = "center"
 FONT = ("Arial", 24, "normal")
 COLOR = "white"
 class Scoreboard(Turtle):
+    @staticmethod
+    def get_high_score():
+        with open("data.txt") as txt_file:
+            high_score = txt_file.read()
+            return high_score
+
+    @staticmethod
+    def write_high_score(score):
+        with open("data.txt", mode = "w") as txt_file:
+            txt_file.write(score)
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = int(Scoreboard.get_high_score())
         self.color(COLOR)
         self.penup()
         self.goto(0, 260)
         self.hideturtle()
         self.update_scoreboard()
 
+
+
     def update_scoreboard(self):
         self.clear()
         self.write(f"Score: {self.score} High Score: {self.high_score}", align = ALIGNMENT, font = FONT)
 
     def reset(self):
-        if self.score> self.high_score:
+        if self.score > self.high_score:
             self.high_score = self.score
+            Scoreboard.write_high_score(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 

3b52666483c938f7025a56cd058e9eb4e4c483df
luciancristialx <alexandruluciancristi@gmail.com>
2024-02-09 17:36:49 +0200

    File paths

--- Day24_Files_Directories_Paths/main.py
+++ Day24_Files_Directories_Paths/main.py
@@ -6,13 +6,13 @@
 # txt_file.close()
 
 #Read file
-# with open("my_file.txt") as txt_file:
-#     file_content = txt_file.read()
-#     print(file_content)
+with open("../../../Desktop/Python Learning/my_file.txt") as txt_file:
+    file_content = txt_file.read()
+    print(file_content)
 
 #mode = r (read), w (write), a (append)
-with open("new_file.txt",mode = "w") as txt_file:
-    txt_file.write("New text.")
+# with open("new_file.txt",mode = "w") as txt_file:
+#     txt_file.write("New text.")
 
 
 

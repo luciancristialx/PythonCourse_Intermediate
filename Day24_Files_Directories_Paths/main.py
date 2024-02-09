c843741df9929688250c8df09e3f2457927b33dc
luciancristialx <alexandruluciancristi@gmail.com>
2024-02-09 15:19:26 +0200

    File manipulation - read, write and append

--- Day24_Files_Directories_Paths/main.py
+++ Day24_Files_Directories_Paths/main.py
@@ -1,6 +1,18 @@
-txt_file = open("my_file.txt")
+# txt_file = open("my_file.txt")
+#
+# file_content = txt_file.read()
+# print(file_content)
+#
+# txt_file.close()
 
-file_content = txt_file.read()
-print(file_content)
+#Read file
+# with open("my_file.txt") as txt_file:
+#     file_content = txt_file.read()
+#     print(file_content)
 
-txt_file.close()
+#mode = r (read), w (write), a (append)
+with open("new_file.txt",mode = "w") as txt_file:
+    txt_file.write("New text.")
+
+
+
